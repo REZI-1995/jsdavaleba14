5914e053547e8be7b9025351d3b05f75f9487fd6
REZI-1995 <rekapanadze@gmail.com>
2024-04-05 16:58:28 +0400

    meore davaleba

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 let humansAge = [18,19,20,21,22]
 console.log (((humansAge[0]) + (humansAge[1]) + (humansAge[2]) + (humansAge[3]) + (humansAge[4])) / 5);
+
+const students = [
+    {name: 'rezi', age: 29 },
+    {name: 'avto', age: 30 },
+    {name: 'givi', age: 33 },
+    {name: 'vako', age: 31 },
+    {name: 'shako', age: 32 },
+];
+console.log (students)
+
